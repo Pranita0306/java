83e57963b7bbc3bbe09386cad4500a639a7d8df5
Pranita0306 <159889169+Pranita0306@users.noreply.github.com>
2024-08-21 22:20:31 +0530

    Add files via upload

---
+++ SwapNumbers.java
@@ -0,0 +1,29 @@
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.20f, second = 2.45f;
+
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+
+        // using the temporary
+        float temporary = first;
+
+
+        first = second;
+
+        
+        second = temporary;
+
+        //without temporary 
+        // first = first - second;
+        //second = first + second;
+        //first = second - first;
+
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+    }
+}
