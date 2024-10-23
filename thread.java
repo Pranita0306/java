ba02ddec1e234adf76b496c12b453a7fcc5ccac2
Pranita0306 <159889169+Pranita0306@users.noreply.github.com>
2024-10-23 23:05:43 +0530

    Add files via upload

---
+++ thread.java
@@ -0,0 +1,30 @@
+class mythreadrunnable1 implements runnable{
+public void run(){
+for(int i=0; int=50; i++)
+{
+System.out.println(" I am a thread 1 not a thread 1");
+}
+}
+
+class mythreadrunnable2 implements runnable{
+public void run(){
+for(int i=0; int=50; i++)
+{
+System.out.println(" I am a thread 2 not a thread 2");
+}
+}
+
+public class thread{
+public static void main(String[] args){
+mythreadrunnnable1 bullet1 = new mythread();
+thread = run1 = new thread(bllent1);
+
+mythreadrunnnable1 bullet2 = new mythread();
+thread = run1 = new thread(bllent2);
+
+gun1.start();
+gun1.start();
+}
+}
+
+
